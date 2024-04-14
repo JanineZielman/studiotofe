e9fa84d5e91b336069ad58696ba9c4c2a2bcc773
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2024-04-14 19:21:07 +0200

    works

--- pages/works.js
+++ pages/works.js
@@ -5,6 +5,8 @@ import * as prismicH from "@prismicio/helpers";
 import { createClient } from "../prismicio";
 import { components } from "../slices/";
 import { Layout } from "../components/Layout";
+import { PrismicNextImage } from "@prismicio/next";
+
 
 const Works = ({ page, navigation, settings, projects }) => {
   console.log(page)
@@ -14,7 +16,18 @@ const Works = ({ page, navigation, settings, projects }) => {
       <Head>
         <title>{prismicH.asText(page.data.title)}</title>
       </Head>
-      
+        <div className="projects">
+          {page.data.works.map((item, i) => {
+            return(
+              <div className="project-item" key={`project${i}`}>
+                <p>{item.work.data.title}</p>
+                <span>{item.work.data.year}</span>
+                <img src={item.work.data.cover_image.url}/>
+              </div>
+            )
+          })}
+        </div>
+
       <SliceZone slices={page.data.slices} components={components} />
     </Layout>
   );
@@ -26,7 +39,7 @@ export async function getStaticProps({ previewData }) {
   const client = createClient({ previewData });
 
   const page = await client.getSingle("works", {
-    fetchLinks: `project.title, project.intro, project.image `
+    fetchLinks: `project.title, project.intro, project.cover_image, project.year `
   });
   const projects = await client.getAllByType("project");
   const navigation = await client.getSingle("navigation");
