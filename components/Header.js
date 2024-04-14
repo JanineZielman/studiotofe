051bc5088d35557b9bf55581f39c836a6d5547ea
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2024-04-14 12:16:32 +0200

    fix

--- components/Header.js
+++ components/Header.js
@@ -2,14 +2,14 @@ import { PrismicLink, PrismicText } from "@prismicio/react";
 import * as prismicH from "@prismicio/helpers";
 import React from "react";
 import { useRouter } from "next/router";
-
+import Link from "next/link";
 
 export const Header = ({ navigation, settings }) => {
 	const router = useRouter();
 
   return (
     <header className={`${router.asPath == '/' && 'home'}`}>
-			<a href="/"><div className="site-title"></div></a>
+			<Link href="/"><a className="site-title"></a></Link>
       <div className="menu">
 				{navigation.data?.links.map((item) => (
 					<PrismicLink field={item.link} key={prismicH.asText(item.label)}>
